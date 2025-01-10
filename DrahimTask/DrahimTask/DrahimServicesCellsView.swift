5bbe07cefc7e508a5ed479b0af460814ba83d37b
devAshwaq <88581485+engAshwaqAlmunmms@users.noreply.github.com>
2025-01-11 01:12:58 +0300

    cell view

---
+++ DrahimTask/DrahimTask/DrahimServicesCellsView.swift
@@ -0,0 +1,34 @@
+//
+//  DrahimServicesCellsView.swift
+//  DrahimTask
+//
+//  Created by Ashwaq Alghamdi on 10.01.2025.
+//
+
+import SwiftUI
+
+struct DrahimServicesCellsView: View {
+    
+    @Binding var serviceImage: String
+    @Binding var serviceTitle: String
+    @Binding var serviceSubTitle: String
+    @Binding var appearance: Appearance
+    @Environment(\.colorScheme) var colorScheme
+
+    var body: some View {
+        HStack {
+            Image(uiImage: UIImage(named:"\(serviceImage).\(appearance.rawValue)") ?? UIImage())
+            VStack(alignment: .leading) {
+                Text(serviceTitle)
+                    .foregroundStyle(Color.black)
+                    .bold()
+                Text(serviceSubTitle)
+                    .foregroundStyle(Color.gray)
+            }
+            Spacer()
+            NavigationLink("") {
+                WelcomeView()
+            }
+        }
+    }
+}
