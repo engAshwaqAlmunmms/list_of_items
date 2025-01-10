529fc293c7c7deb8dca264fe554bb8c65b809d13
devAshwaq <88581485+engAshwaqAlmunmms@users.noreply.github.com>
2025-01-11 01:10:51 +0300

    create welcome view

---
+++ DrahimTask/DrahimTask/WelcomeView.swift
@@ -0,0 +1,14 @@
+//
+//  WelcomeView.swift
+//  DrahimTask
+//
+//  Created by Ashwaq Alghamdi on 10.01.2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
