ec44b056c5657c0111dec586398a60a68114ff42
devAshwaq <88581485+engAshwaqAlmunmms@users.noreply.github.com>
2025-01-10 03:35:30 +0300

    create new project

---
+++ DrahimTask/DrahimTask/DrahimTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrahimTaskApp.swift
+//  DrahimTask
+//
+//  Created by Ashwaq Alghamdi on 10.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct DrahimTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
